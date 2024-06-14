19435e66120d971781c29d7baaa64c051eb80c9e
Bruno Forti <139017911+Bforti@users.noreply.github.com>
2024-06-14 03:38:12 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -35,6 +35,32 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+
+
+
+
+
+@app.route("/users", methods=["POST"])
+def create():
+    data = request.get_json()
+    email = data["email"]
+    
+    repetido = User.query.filter_by(email=email).first()
+    if repetido: 
+        return jsonify({"error":"correo registrado"}), 400
+    
+    password = data["password"]
+
+    user = User(email=email,password=password,is_active=True)
+    
+    db.session.add(user)
+    db.session.commit()   
+    return jsonify({"mensaje":"registro exitoso"}),201
+
+
+
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
@@ -45,6 +71,30 @@ def get_users():
         all_u.append(serialized_user)
 
     return jsonify(all_u), 200
+
+
+@app.route('/users/favorites', methods=['GET'])
+def get_list_favorites():
+    data=request.get_json()
+    email = data["email"]
+    user_query = User.query.filter_by(email=email).first()
+    user_id=user_query.id
+
+    favorite_people = UPeopleFavorite.query.filter_by(user_id=user_id).all()
+    favorite_planet = UPlanetFavorite. query.filter_by(user_id=user_id).all()
+    results_character = list(map(lambda item: item.serialize(), favorite_people))
+    results_planet = list(map(lambda item: item.serialize(), favorite_planet))
+    
+    if results_character == [] and results_planet == []:
+        return jsonify({"msg": "favorites not found"}), 404
+    response_body = {
+        "msg": "ok",
+        "results": [results_character, results_planet],
+        }
+    return jsonify(response_body), 200
+
+
+
 
 @app.route('/people', methods=['GET'])
 def get_people():
@@ -58,6 +108,7 @@ def get_person(people_id):
     person = People.query.get_or_404(people_id)
     
     return jsonify(person.serialize()), 200
+
 
 @app.route('/people', methods=['POST'])
 def add_person():
@@ -91,99 +142,103 @@ def get_planet(planet_id):
     
     return jsonify(planet.serialize()), 200
 
-@app.route('/users/favorites', methods=['GET'])
-def get_user_favorites():
-    user_id = request.json.get('user_id')  
 
-    if user_id is None:
-        return jsonify({"error": "User ID is required."}), 400
-    
-    user = User.query.get(user_id)
 
-    if user is None:
-        return jsonify({"error": "User not found."}), 404
-    
-    favorites = user.favorites
-    
-    serialized_favorites = [favorite.serialize() for favorite in favorites]
-    
-    return jsonify(serialized_favorites), 200
+
 
 @app.route('/favorite/people/<int:people_id>', methods=['POST'])
-def add_favorite_people(people_id):
-    user_id = request.json.get('user_id')
+def add_fav_character_to_user(people_id):
+    data = request.get_json()
+    email = data.get("email")  # Utiliza get para evitar errores si 'email' no está presente
+    if not email:
+        return jsonify({"error": "Email is required"}), 400
 
-    user = User.query.get(user_id)
-    person = People.query.get(people_id)
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return jsonify({"error": "User not found"}), 404
 
-    favorite_relation = UPeopleFavorite(user_id=user_id, people_id=people_id)
-    db.session.add(favorite_relation)
+    character = People.query.get(people_id)
+    if not character:
+        return jsonify({"error": "Character not found"}), 404
+
+    # Verifica si el usuario ya tiene este personaje como favorito
+    if UPeopleFavorite.query.filter_by(user_id=user.id, people_id=people_id).first():
+        return jsonify({"msg": "Character already exists in favorites"}), 400
+
+    # Agrega el personaje como favorito para el usuario
+    favorite_character = UPeopleFavorite(people_id=people_id, user_id=user.id)
+    db.session.add(favorite_character)
     db.session.commit()
+    
+    return jsonify({"msg": "Character added to favorites"}), 200
 
-    return jsonify({"message": f"Added {person.name} to favorites for user {user.name}"}), 200
 
 @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
-def add_favorite_planets(planet_id):
-    user_id = request.json.get('user_id')
+def add_fav_planet_to_user(planet_id):
+    data = request.get_json()
+    email = data.get("email")
+    if not email:
+        return jsonify({"error": "Email is required"}), 400
 
-    user = User.query.get(user_id)
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
     planet = Planets.query.get(planet_id)
+    if not planet:
+        return jsonify({"error": "Planet not found"}), 404
 
-    favorite_relation = UPlanetFavorite(user_id=user_id, planet_id=planet_id)
-    db.session.add(favorite_relation)
+    # Verifica si el usuario ya tiene este planeta como favorito
+    if UPlanetFavorite.query.filter_by(user_id=user.id, planet_id=planet_id).first():
+        return jsonify({"msg": "Planet already exists in favorites"}), 400
+
+    # Agrega el planeta como favorito para el usuario
+    favorite_planet = UPlanetFavorite(planet_id=planet_id, user_id=user.id)
+    db.session.add(favorite_planet)
     db.session.commit()
+    
+    return jsonify({"msg": "Planet added to favorites"}), 200
 
-    return jsonify({"message": f"Added {planet.name} to favorites for user {user.name}"}), 200
+
+
+
 
 @app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
-def delete_favorite_planet(planet_id):
-    user_id = request.json.get('user_id')  
+def delete_planet(planet_id):
+    data=request.get_json()
+    email = data["email"]
+    user_query = User.query.filter_by(email=email).first()
+    user_id = user_query.id
+    planet_query = Planets.query.filter_by(id=planet_id).first()
+    if planet_query is None:
+        return ({"msg": "the planet doesn't exist"}), 400
+    else:
+        planet_query = UPlanetFavorite.query.filter_by(planet_id=planet_id, user_id=user_id).first()
+    if planet_query:
+        db.session.delete(planet_query)
+        db.session.commit()
+        return jsonify({"msg": "planet was successfully deleted"}), 200
+    else:
+        return jsonify({"msg": "planet not found"}), 404 
     
-    if user_id is None:
-        return jsonify({"error": "User ID is required."}), 400
-    
-    user = User.query.get(user_id)
-    
-    if user is None:
-        return jsonify({"error": "User not found."}), 404
-    
-    favorite_planet = Planets.query.get(planet_id)
-    
-    if favorite_planet is None:
-        return jsonify({"error": "Favorite planet not found."}), 404
-    
-    if favorite_planet not in user.planet_favorites:
-        return jsonify({"error": "Planet is not a favorite of the user."}), 400
-    
-    user.planet_favorites.remove(favorite_planet)
-    db.session.commit()
-    
-    return jsonify({"message": f"Favorite planet {favorite_planet.name} deleted for user {user.name}"}), 200
 
 @app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
-def delete_favorite_person(people_id):
-    user_id = request.json.get('user_id')  
-    
-    if user_id is None:
-        return jsonify({"error": "User ID is required."}), 400
-
-    user = User.query.get(user_id)
-
-    if user is None:
-        return jsonify({"error": "User not found."}), 404
-    
-    favorite_person = People.query.get(people_id)
-    
-    if favorite_person is None:
-        return jsonify({"error": "Favorite person not found."}), 404
-    
-    if favorite_person not in user.people_favorites:
-        return jsonify({"error": "Person is not a favorite of the user."}), 400
-    
-    user.people_favorites.remove(favorite_person)
-    db.session.commit()
-    
-    return jsonify({"message": f"Favorite person {favorite_person.name} deleted for user {user.name}"}), 200
+def delete_people(people_id):
+    data=request.get_json()
+    email = data["email"]
+    user_query = User.query.filter_by(email=email).first()
+    user_id = user_query.id
+    people_query = People.query.filter_by(id=people_id).first()
+    if people_query is None:
+        return ({"msg": "the character doesn't exist"}), 400
+    else:
+        people_query = UPeopleFavorite.query.filter_by(people_id=people_id, user_id=user_id).first()
+    if people_query:
+        db.session.delete(people_query)
+        db.session.commit()
+        return jsonify({"msg": "character was successfully deleted"}), 200
+    else:
+        return jsonify({"msg": "character not found"}), 404 
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,10 +4,19 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favoritos_planet = db.relationship('UPlanetFavorite', primaryjoin='User.id == UPlanetFavorite.user_id', backref='user', lazy=True)
+    favoritos_people = db.relationship('UPeopleFavorite', backref='user', lazy=True)
+  
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "email":self.email        }
 
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +25,8 @@ class People(db.Model):
     weight = db.Column(db.String(10), nullable=True)
     gender = db.Column(db.String(10), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    favoritos_character = db.relationship('UPeopleFavorite', backref='character', lazy=True)
+
 
     def serialize(self):
         return {
@@ -33,6 +44,7 @@ class Planets(db.Model):
     terrain = db.Column(db.String(10), nullable=True)
     resources = db.Column(db.String(10), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    favoritos_planet = db.relationship('UPlanetFavorite', backref='planet', lazy=True)
 
     def serialize(self):
         return {
@@ -44,15 +56,36 @@ class Planets(db.Model):
         }
     
 class UPeopleFavorite(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), primary_key=True)
+    __tablename__ = "UPeopleFavorite"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+
+
+    def __repr__(self):
+        return '<UPeopleFavorite %r>' % self.id
 
     def serialize(self):
+        result = People.query.filter_by(id=self.people_id).first()
         return {
+            "id": self.id,
             "user_id": self.user_id,
-            "people_id": self.people_id
+            "people_id": result.serialize()["name"], 
         }
-
 class UPlanetFavorite(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), primary_key=True)
+    __tablename__ = "UPlanetFavorite"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    def __repr__(self):
+        return '<UPlanetFavorite %r>' % self.id
+
+    def serialize(self):
+        result = Planets.query.filter_by(id=self.planets_id).first()
+
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planets_id": result.serialize()["name"],
+        }
